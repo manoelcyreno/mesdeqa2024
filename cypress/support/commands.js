a313dc79ccc81d274666e10a2c2589f00f02449f
Manoel Pereira <manoel.pereira@abi.ai>
2024-06-18 15:31:02 +0200

    Remove unnecessary code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -22,20 +22,3 @@
 //
 //
 // -- This will overwrite an existing command --
-
-import '@testing-library/cypress/add-commands'
-
-Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
-  if (options && options.sensitive) {
-    // turn off original log
-    options.log = false
-    // create our own log with masked message
-    Cypress.log({
-      $el: element,
-      name: 'type',
-      message: '*'.repeat(text.length),
-    })
-  }
-
-  return originalFn(element, text, options)
-})
